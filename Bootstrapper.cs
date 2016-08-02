44bf3e75fbd8601ff1755d923025a9d299cdbe5b
CodeAngel247 <emily.f.svensson@hotmail.com>
2016-08-01 20:03:21 -0500

    Starting To Set Up Container

--- Bootstrapper.cs
+++ Bootstrapper.cs
@@ -9,6 +9,19 @@ namespace Demo
 {
     public class Bootstrapper
     {
-    
+        private AssemblyCatalog catalog;
+
+        public Bootstrapper()
+        {
+            Compose();
+        }
+
+        private void Compose()
+        {
+            AssemblyCatalog catalog = new AssemblyCatalog(System.Reflection.Assembly.GetExecutingAssembly());
+            CompositionContainer container = new CompositionContainer(catalog);
+            //container.SatisfyImportsOnce(catalog.CreateCompositionService());
+        }
+
     }
 }
