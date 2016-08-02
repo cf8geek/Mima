7a3db41d930135abed258995f888f6f8a24a051d
CodeAngel247 <emily.f.svensson@hotmail.com>
2016-08-02 05:46:40 -0500

    Create MessageCenter.cs

---
+++ MessageCenter.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+
+namespace Demo
+{
+
+    [Export(typeof(Demo.Common.IMessageCenter))]
+    public class MessageCenter : Demo.Common.IMessageCenter
+    {
+
+        public string SendMessage()
+        {
+            return "This is a default message.";
+        }
+    }
+}
